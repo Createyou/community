8e0ff9045f320aafe9c9479c8e5197cdb8c30bcb
Createyou <841704530@qq.com>
2020-01-29 12:10:37 +0800

    sql

--- src/main/java/com/jn/demo/Authorize.java
+++ src/main/java/com/jn/demo/Authorize.java
@@ -2,6 +2,8 @@ package com.jn.demo;
 
 import com.jn.demo.dto.AccesssTokenDTO;
 import com.jn.demo.dto.GithubUser;
+import com.jn.demo.mapper.UserMapper;
+import com.jn.demo.model.User;
 import com.jn.demo.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,19 +12,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 @Controller
 public class Authorize {
+
     @Autowired
     private GithubProvider githubProvider;
+
     @Value("${github.client.id}")
     private String clientId;
     @Value("${github.client.secret}")
     private String clientSecret;
     @Value("${github.redirect.uri}")
     private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public  String callback(@RequestParam(name = "code") String code,
-                            @RequestParam(name="state") String state){
+                            @RequestParam(name="state") String state,
+                            HttpServletRequest request){
         AccesssTokenDTO accesssTokenDTO = new AccesssTokenDTO();
         accesssTokenDTO.setClient_id(clientId);
         accesssTokenDTO.setClient_secret(clientSecret);
@@ -30,9 +42,25 @@ public class Authorize {
         accesssTokenDTO.setRedirect_uri(redirectUri );
         accesssTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accesssTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser!=null){
+            User user=new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtcreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtcreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("githubUser",githubUser);
+            return "redirect:/";
+            //登陆成功,写cookie 和session
+
+        }
+        else{
+            return "redirect:/";
+            //登陆失败 ,重新登陆
+        }
+
 
     }
 
---
+++ src/main/java/com/jn/demo/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.jn.demo.mapper;
+
+import com.jn.demo.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
